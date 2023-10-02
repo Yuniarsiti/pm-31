950360dccc42547ab1221633b47964686e9c59e6
Yuniarsiti <141316968+Yuniarsiti@users.noreply.github.com>
2023-10-02 13:45:24 +0700

    Add files via upload

---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function profile  (namaDepan, namaBelakang, email){
+  console.log("Nama : " + namaDepan +" "+ namaBelakang)
+  console.log("Email " + email)
+
+}
+
+profile(" Yuniar", "nurhaliza", "yuniarsitinur@gmail.com")
